5394664a80e35f30cfd0be1d182629e34f5680f3
Arif <faizin.arief@gmail.com>
2020-08-16 11:29:47 +0700

    Add: Mapbox

--- maps/src/main/java/com/dicoding/tourismapp/maps/MapsActivity.kt
+++ maps/src/main/java/com/dicoding/tourismapp/maps/MapsActivity.kt
@@ -1,28 +1,53 @@
 package com.dicoding.tourismapp.maps
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.Observer
 import com.dicoding.tourismapp.core.data.Resource
+import com.dicoding.tourismapp.core.domain.model.Tourism
+import com.dicoding.tourismapp.detail.DetailTourismActivity
 import com.dicoding.tourismapp.maps.di.mapsModule
+import com.google.gson.Gson
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.geometry.LatLngBounds
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 import kotlinx.android.synthetic.main.activity_maps.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.context.loadKoinModules
 
 class MapsActivity : AppCompatActivity() {
 
+    companion object {
+        private const val ICON_ID = "ICON_ID"
+    }
+
+    private lateinit var mapboxMap: MapboxMap
+
     private val mapsViewModel: MapsViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+
         setContentView(R.layout.activity_maps)
 
         loadKoinModules(mapsModule)
-
         supportActionBar?.title = "Tourism Map"
 
-        getTourismData()
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { mapboxMap ->
+            this.mapboxMap = mapboxMap
+            getTourismData()
+        }
     }
 
     private fun getTourismData() {
@@ -32,7 +57,7 @@ class MapsActivity : AppCompatActivity() {
                     is Resource.Loading -> progress_bar.visibility = View.VISIBLE
                     is Resource.Success -> {
                         progress_bar.visibility = View.GONE
-                        tv_maps.text = "This is map of ${tourism.data?.get(0)?.name}"
+                        showMarker(tourism.data)
                     }
                     is Resource.Error -> {
                         progress_bar.visibility = View.GONE
@@ -42,5 +67,37 @@ class MapsActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    private fun showMarker(dataTourism: List<Tourism>?) {
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+            style.addImage(ICON_ID, BitmapFactory.decodeResource(resources, R.drawable.mapbox_marker_icon_default))
+            val latLngBoundsBuilder = LatLngBounds.Builder()
+
+            val symbolManager = SymbolManager(mapView, mapboxMap, style)
+            symbolManager.iconAllowOverlap = true
+
+            val options = ArrayList<SymbolOptions>()
+            dataTourism?.forEach { data ->
+                latLngBoundsBuilder.include(LatLng(data.latitude, data.longitude))
+                options.add(
+                    SymbolOptions()
+                        .withLatLng(LatLng(data.latitude, data.longitude))
+                        .withIconImage(ICON_ID)
+                        .withData(Gson().toJsonTree(data))
+                )
+            }
+            symbolManager.create(options)
+
+            val latLngBounds = latLngBoundsBuilder.build()
+            mapboxMap.easeCamera(CameraUpdateFactory.newLatLngBounds(latLngBounds, 50), 5000)
+
+            symbolManager.addClickListener { symbol ->
+                val data = Gson().fromJson(symbol.data, Tourism::class.java)
+                val intent = Intent(this, DetailTourismActivity::class.java)
+                intent.putExtra(DetailTourismActivity.EXTRA_DATA, data)
+                startActivity(intent)
+            }
+        }
     }
 }
