2fcb7f75e173d691c8da0e958489419ddd917bc5
Arif <faizin.arief@gmail.com>
2021-05-06 15:16:31 +0700

    Update Hilt to Beta (SingletonComponent & HiltViewModel)

--- app/src/main/java/com/dicoding/tourismapp/di/AppModule.kt
+++ app/src/main/java/com/dicoding/tourismapp/di/AppModule.kt
@@ -6,12 +6,15 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(ViewModelComponent::class)
 abstract class AppModule {
 
     @Binds
+    @ViewModelScoped
     abstract fun provideTourismUseCase(tourismInteractor: TourismInteractor): TourismUseCase
 
 }
