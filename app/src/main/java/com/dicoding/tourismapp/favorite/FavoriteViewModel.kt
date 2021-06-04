dced855461b233c9784645e26d0a6beb11a792a9
Arif <faizin.arief@gmail.com>
2021-06-04 16:39:32 +0700

    Update Hilt ke Versi Stable

--- app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/dicoding/tourismapp/favorite/FavoriteViewModel.kt
@@ -1,6 +1,5 @@
 package com.dicoding.tourismapp.favorite
 
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
